73bfa997a1d2b16f776860a599473899c5f3348d
Ivan Sinek <isinek@gmail.com>
2024-04-20 15:59:19 +0200

    crkbd/keymaps/isinek: miryoku-like QWERTY

--- keyboards/crkbd/keymaps/isinek/config.h
+++ keyboards/crkbd/keymaps/isinek/config.h
@@ -20,15 +20,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-
 #define MASTER_LEFT
 
 #define OLED_FONT_H     "keyboards/crkbd/lib/glcdfont.c"
 #define OLED_TIMEOUT    120000
 #define OLED_BRIGHTNESS 120
-#define TAPPING_TERM    175
 
 #define SPLIT_WPM_ENABLE
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
+
+#define TAPING_TERM 120
+
+// Mouse key speed and acceleration.
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY          0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL       16
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY    0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED      6
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX    64
 
 #undef RGBLIGHT_ENABLE
 #undef RGB_MATRIX_ENABLE
